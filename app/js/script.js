321cd8c71a3a2a5c9cc672c3d69e591e0f36cbc2
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-02-19 19:55:40 +0100

    my first commit

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+// const range = document.querySelector(".slider");
+// const month = document.querySelector(".price");
+// const view = document.querySelector(".pageview");
+
+// // month.innerHTML = range.value;
+// // view.innerHTML = range.value;
+
+// range.oninput = function () {
+//    month.innerHTML = this.value;
+// };
+// // range.oninput = function () {
+// //    view.innerHTML = this.value;
+// // };
