79b2187696758a7426ce82ecba8d6a483f7606f6
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-02-23 01:11:21 +0100

    added activity for the mobile view slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,77 +1,114 @@
 let range = document.querySelector(".slider");
 let month = document.querySelector(".price");
 let view = document.querySelector(".pageview");
-const toggle = document.getElementById("toggle");
 
+let rangeMobile = document.getElementById("range");
+let monthMobile = document.getElementById("price");
+let viewMobile = document.getElementById("pageview");
+
+const toggle = document.getElementById("toggle");
 
 //the toggle button
 const toggleClick = function () {
    toggle.classList.toggle("active");
 };
 
-//range slider color toggler
-
-
-// const entry = (val) => {
-//    if (val === 1) {
-//       view.innerHTML = '10k'
-//    }
-// };
-
-// range.oninput = function () {
-//    month.innerHTML = this.value;
-// };
-
-
-// the range slider
+// the range slider for desktop
 const rangeValue = function () {
    let val = range.value;
-   
-   if (toggle.classList.contains("active")) {
-   // console.log("discount");
-   if (val == 1) {
-      view.innerHTML = "10k";
-      month.innerHTML = "$2.00";
-   } else if (val == 2) {
-      view.innerHTML = "50k";
-      month.innerHTML = "$3.00";
-   } else if (val == 3) {
-      view.innerHTML = "100k";
-      month.innerHTML = "$4.00";
-   } else if (val == 4) {
-      view.innerHTML = "500k";
-      month.innerHTML = "$6.00";
-   } else if (val == 5) {
-      view.innerHTML = "1M";
-      month.innerHTML = "$9.00";
-   }
-} else {
-   // console.log("no discount");
 
-   if (val == 1) {
-      view.innerHTML = "10k";
-      month.innerHTML = "$8.00";
-   } else if (val == 2) {
-      view.innerHTML = "50k";
-      month.innerHTML = "$12.00";
-   } else if (val == 3) {
-      view.innerHTML = "100k";
-      month.innerHTML = "$16.00";
-   } else if (val == 4) {
-      view.innerHTML = "500k";
-      month.innerHTML = "$24.00";
-   } else if (val == 5) {
-      view.innerHTML = "1M";
-      month.innerHTML = "$36.00";
+   if (toggle.classList.contains("active")) {
+      // console.log("discount");
+      if (val == 1) {
+         view.innerHTML = "10k";
+         month.innerHTML = "$2.00";
+      } else if (val == 2) {
+         view.innerHTML = "50k";
+         month.innerHTML = "$3.00";
+      } else if (val == 3) {
+         view.innerHTML = "100k";
+         month.innerHTML = "$4.00";
+      } else if (val == 4) {
+         view.innerHTML = "500k";
+         month.innerHTML = "$6.00";
+      } else if (val == 5) {
+         view.innerHTML = "1M";
+         month.innerHTML = "$9.00";
+      }
+   } else {
+      // console.log("no discount");
+
+      if (val == 1) {
+         view.innerHTML = "10k";
+         month.innerHTML = "$8.00";
+      } else if (val == 2) {
+         view.innerHTML = "50k";
+         month.innerHTML = "$12.00";
+      } else if (val == 3) {
+         view.innerHTML = "100k";
+         month.innerHTML = "$16.00";
+      } else if (val == 4) {
+         view.innerHTML = "500k";
+         month.innerHTML = "$24.00";
+      } else if (val == 5) {
+         view.innerHTML = "1M";
+         month.innerHTML = "$36.00";
+      }
    }
-}
 };
 
-['click', 'input'].forEach(e => {
-   range.addEventListener(e, rangeValue);
-})
+// the range slider for mobile
+const rangeValueMobile = function () {
+   let valM = rangeMobile.value;
 
-// range.addEventListener("input", rangeValue);
+   if (toggle.classList.contains("active")) {
+      // console.log("discount");
+      if (valM == 1) {
+         viewMobile.innerHTML = "10k";
+         monthMobile.innerHTML = "$2.00";
+      } else if (valM == 2) {
+         viewMobile.innerHTML = "50k";
+         monthMobile.innerHTML = "$3.00";
+      } else if (valM == 3) {
+         viewMobile.innerHTML = "100k";
+         monthMobile.innerHTML = "$4.00";
+      } else if (valM == 4) {
+         viewMobile.innerHTML = "500k";
+         monthMobile.innerHTML = "$6.00";
+      } else if (valM == 5) {
+         viewMobile.innerHTML = "1M";
+         monthMobile.innerHTML = "$9.00";
+      }
+   } else {
+      // console.log("no discount");
+
+      if (valM == 1) {
+         viewMobile.innerHTML = "10k";
+         monthMobile.innerHTML = "$8.00";
+      } else if (valM == 2) {
+         viewMobile.innerHTML = "50k";
+         monthMobile.innerHTML = "$12.00";
+      } else if (valM == 3) {
+         viewMobile.innerHTML = "100k";
+         monthMobile.innerHTML = "$16.00";
+      } else if (valM == 4) {
+         viewMobile.innerHTML = "500k";
+         monthMobile.innerHTML = "$24.00";
+      } else if (valM == 5) {
+         viewMobile.innerHTML = "1M";
+         monthMobile.innerHTML = "$36.00";
+      }
+   }
+};
+
+// ['click', 'input'].forEach(e => {
+//    range.addEventListener(e, rangeValue());
+// })
+
+// ['click', 'input'].forEach(e => {
+//    rangeMobile.addEventListener(e, rangeValueMobile);
+// })
+
+range.addEventListener("input", rangeValue);
+rangeMobile.addEventListener("input", rangeValueMobile);
 toggle.addEventListener("click", toggleClick);
-
-// const entry = []
